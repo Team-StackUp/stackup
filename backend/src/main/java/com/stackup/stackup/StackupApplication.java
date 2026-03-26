7e492c74f0739f4c4bffa915b3f3c329f783acdc
pswaao88 <pswaao88@gmail.com>
2026-03-26 17:21:11 +0900

    chore: 백엔드 core 서버 의존성 추가
    spring boot 4.0.4, web, jpa, lombok, queryDSL, lombok 추가

---
+++ backend/src/main/java/com/stackup/stackup/StackupApplication.java
@@ -0,0 +1,13 @@
+package com.stackup.stackup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackupApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/stackup/stackup/StackupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackup.stackup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
