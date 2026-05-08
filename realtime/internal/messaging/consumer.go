e312e147592c58f193d10361836a64c94c9d4fde
SeoHyeon <tjgus1668@naver.com>
2026-05-08 22:39:42 +0900

    feat(realtime): AMQP 연결 및 재연결 컨슈머 추가

---
+++ realtime/internal/messaging/consumer.go
@@ -0,0 +1,84 @@
+package messaging
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Handler interface {
+	Handle(body []byte) error
+}
+
+type Consumer struct {
+	Connector *Connector
+	QueueName string
+	Handler   Handler
+}
+
+// Run blocks, reconnecting on connection loss until ctx is cancelled.
+func (c *Consumer) Run(ctx context.Context) error {
+	for {
+		if err := c.runOnce(ctx); err != nil {
+			if ctx.Err() != nil {
+				return ctx.Err()
+			}
+			slog.Warn("amqp.session.reconnecting", "err", err)
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case <-time.After(2 * time.Second):
+			}
+			continue
+		}
+		return nil
+	}
+}
+
+func (c *Consumer) runOnce(ctx context.Context) error {
+	conn, err := c.Connector.Dial(ctx)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	if _, err := ch.QueueDeclarePassive(c.QueueName, true, false, false, false, nil); err != nil {
+		return err
+	}
+
+	deliveries, err := ch.Consume(c.QueueName, "", false, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+	slog.Info("amqp.consumer.started", "queue", c.QueueName)
+
+	closeCh := conn.NotifyClose(make(chan *amqp.Error, 1))
+
+	for {
+		select {
+		case <-ctx.Done():
+			slog.Info("amqp.consumer.stopping")
+			return ctx.Err()
+		case err := <-closeCh:
+			return err
+		case d, ok := <-deliveries:
+			if !ok {
+				return amqp.ErrClosed
+			}
+			if err := c.Handler.Handle(d.Body); err != nil {
+				slog.Warn("amqp.handler.failed", "err", err)
+				_ = d.Nack(false, false) // drop (no requeue)
+				continue
+			}
+			_ = d.Ack(false)
+		}
+	}
+}
